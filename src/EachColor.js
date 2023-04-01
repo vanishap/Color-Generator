a18a86ac33a69dbb44372fd67755572b60d46ad2
vanishap <vanishareddyp@gmail.com>
2023-03-31 22:20:22 -0700

    initial

---
+++ src/EachColor.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EachColor = () => {
+  return (
+    <div>
+      I'm am neutral
+    </div>
+  )
+}
+
+export default EachColor;
---
+++ src/Form.js
@@ -0,0 +1,16 @@
+const Form = ()=>{
+    return (
+        <div>
+            <form>
+                <label>Name</label>
+                    <input/>
+                
+                <label>ColorName:</label>
+                    <input/>
+                
+                
+            </form>
+        </div>
+    )
+}
+export default Form;
